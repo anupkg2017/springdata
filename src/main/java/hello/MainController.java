89abc21f9ec78821f99f408319927b3b97abd610
Anup Kanoor Gopalakrishnan <agopalakrishnan@manh.com>
2017-09-30 23:33:11 -0400

    second commit

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
 	@GetMapping(path="/add") // Map ONLY GET Requests
 	public @ResponseBody String addNewUser (@RequestParam String name
 			, @RequestParam String email) {
-		// @ResponseBody means the returned String is the response, not a view name
+
 		// @RequestParam means it is a parameter from the GET or POST request
 		
 		User n = new User();
